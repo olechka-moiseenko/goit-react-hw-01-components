4f04f902f3be7fc2710bdbaf297d571a1d527e5a
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-06-30 23:51:49 +0300

    стили для 4 задания

--- src/components/transactionsHistory/transactions.js
+++ src/components/transactionsHistory/transactions.js
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
+import styles from "./transactions.module.css";
 
 export default function TransactionHistory({ items }) {
   return (
-    <table class="transaction-history">
+    <table className={styles.transactionHistory}>
       <thead>
         <tr>
           <th>Type</th>
