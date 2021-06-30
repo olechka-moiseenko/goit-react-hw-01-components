f5dccf3fb78efc49a31e32017ab08f27db039b60
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-06-30 20:00:11 +0300

    Добавила стили для profile.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 //data
-import user from "./components/user.json";
+import user from "./components/profile/user.json";
 import statisticalData from "./components/statistics/statistical-data.json";
 import friends from "./components/friendList/friends.json";
 import transactions from "./components/transactionsHistory/transactions.json";
 //components
-import Profile from "./components/profile";
+import Profile from "./components/profile/profile";
 import Statistics from "./components/statistics/statistics";
 import FriendList from "./components/friendList/friendList";
 import TransactionHistory from "./components/transactionsHistory/transactions";
