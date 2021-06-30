35766a592a9a769ff9492fad0134e58a4b80e69c
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-06-30 23:01:26 +0300

    Добавила стили для friendsList

--- src/components/friendList/friendList.js
+++ src/components/friendList/friendList.js
@@ -1,13 +1,22 @@
 import PropTypes from "prop-types";
-
+import styles from "./friendList.module.css";
 export default function FriendList({ friends }) {
   return (
-    <ul class="friend-list">
+    <ul className={styles.friendList}>
       {friends.map((friend) => (
-        <li class="item" key={friend.id}>
-          <span class="status">{friend.isOnline}</span>
-          <img class="avatar" src={friend.avatar} alt="" width="48" />
-          <p class="name">{friend.name}</p>
+        <li className={styles.item} key={friend.id}>
+          <span
+            className={
+              friend.isOnline ? styles.statusOnline : styles.statusOffline
+            }
+          ></span>
+          <img
+            className={styles.avatar}
+            src={friend.avatar}
+            alt=""
+            width="48"
+          />
+          <p className={styles.name}>{friend.name}</p>
         </li>
       ))}
     </ul>
